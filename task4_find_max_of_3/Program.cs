0a7f894701e09732f0ca155152e6f9cdecd5072c
Nastiona <A.N.Dontsova@yandex.ru>
2022-09-20 17:40:02 +0300

    added task4_find_max_of_3 program

---
+++ task4_find_max_of_3/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа 
+и выдаёт максимальное из этих чисел */
+
+Console.Write("Введи первое число: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Введи второе число: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Введи третье число: ");
+double c = double.Parse(Console.ReadLine());
+
+double max = a;
+
+if (b > max)
+{
+max = b;
+}
+
+if (c > max)
+{
+max = c;
+}
+
+Console.Write($"Максимальным из чисел {a}, {b} и {c} будет число {max}");
