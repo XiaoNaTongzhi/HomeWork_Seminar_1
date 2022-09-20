675f7d7113c14fd3c84871023ba96e1dda946037
Nastiona <A.N.Dontsova@yandex.ru>
2022-09-20 12:51:22 +0300

    added task2_find_max_of_2 program

---
+++ task2_find_max_of_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое большее, а какое меньшее.
+
+Console.Write("Введи первое число: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Введи второе число: ");
+double b = double.Parse(Console.ReadLine());
+
+if (a == b) 
+{
+Console.Write(a);
+Console.Write(" = ");
+Console.Write(b);
+}
+else
+{
+if (a > b) 
+{
+Console.Write(a);
+Console.Write(" > ");
+Console.Write(b);
+}
+else 
+{
+Console.Write(a);
+Console.Write(" < ");
+Console.Write(b);
+}
+}
